5df87748b0cce86168a5445beaa02eb7900441de
undefined <undefined>
2021-02-04 00:44:47 +0800

    style: popover

--- src/docs/popover/demo/hover-with-click.vue
+++ src/docs/popover/demo/hover-with-click.vue
@@ -1,13 +1,3 @@
-<cn>
-#### 悬停点击弹出窗口
-以下示例显示如何创建可悬停和单击的弹出窗口。
-</cn>
-
-<us>
-#### Hover with click popover
-The following example shows how to create a popover which can be hovered and clicked.
-</us>
-
 <docs>
 ---
 order: 4
