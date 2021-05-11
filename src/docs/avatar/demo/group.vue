3bbf5771a96466cda132bd174228dbe2cb5f9938
John <John60676@qq.com>
2021-05-11 17:37:51 +0800

    docs(avatar): add avatar group demo

---
+++ src/docs/avatar/demo/group.vue
@@ -0,0 +1,74 @@
+<docs>
+---
+order: 4
+title:
+  zh-CN: Avatar.Group
+  en-US: Avatar.Group
+---
+
+## zh-CN
+
+头像组合展现。
+
+## en-US
+
+Avatar group display.
+</docs>
+
+<template>
+  <a-avatar-group>
+    <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+    <a-avatar style="background-color: #f56a00">K</a-avatar>
+    <Tooltip title="Ant User" placement="top">
+      <a-avatar style="background-color: #87d068">
+        <template #icon><UserOutlined /></template>
+      </a-avatar>
+    </Tooltip>
+    <a-avatar style="background-color: #1890ff">
+      <template #icon><UserOutlined /></template>
+    </a-avatar>
+  </a-avatar-group>
+  <Divider />
+  <a-avatar-group :max-count="2" :max-style="{ color: '#f56a00', backgroundColor: '#fde3cf' }">
+    <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+    <a-avatar style="background-color: #1890ff">K</a-avatar>
+    <Tooltip title="Ant User" placement="top">
+      <a-avatar style="background-color: #87d068">
+        <template #icon><UserOutlined /></template>
+      </a-avatar>
+    </Tooltip>
+    <a-avatar style="background-color: #1890ff">
+      <template #icon><UserOutlined /></template>
+    </a-avatar>
+  </a-avatar-group>
+  <Divider />
+  <a-avatar-group
+    :max-count="2"
+    size="large"
+    :max-style="{
+      color: '#f56a00',
+      backgroundColor: '#fde3cf',
+    }"
+  >
+    <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+    <a-avatar style="background-color: #1890ff">K</a-avatar>
+    <Tooltip title="Ant User" placement="top">
+      <a-avatar style="background-color: #87d068">
+        <template #icon><UserOutlined /></template>
+      </a-avatar>
+    </Tooltip>
+    <a-avatar style="background-color: #1890ff">
+      <template #icon><UserOutlined /></template>
+    </a-avatar>
+  </a-avatar-group>
+</template>
+
+<script lang="ts">
+import { UserOutlined } from '@ant-design/icons-vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  components: {
+    UserOutlined,
+  },
+});
+</script>
--- src/docs/avatar/index.zh-CN.md
+++ src/docs/avatar/index.zh-CN.md
@@ -14,12 +14,25 @@ cover: https://gw.alipayobjects.com/zos/antfincdn/aBcnbw68hP/Avatar.svg
 
 ## API
 
-| 参数 | 说明 | 类型 | 默认值 |
-| --- | --- | --- | --- |
+### Avatar
+
+| 参数 | 说明 | 类型 | 默认值 | 版本 |
+| --- | --- | --- | --- | --- |
 | icon | 设置头像的图标类型，可设为 Icon 的 `type` 或 VNode | VNode \| slot | - |
-| shape | 指定头像的形状 | Enum{ 'circle', 'square' } | `circle` |
-| size | 设置头像的大小 | number \| Enum{ 'large', 'small', 'default' } | `default` |
+| shape | 指定头像的形状 | `circle` \| `square` | `circle` |  |
+| size | 设置头像的大小 | number \| `large` \| `small` \| `default` \| { xs: number, sm: number, ...} | `default` | 3.x |
 | src | 图片类头像的资源地址 | string | - |
 | srcset | 设置图片类头像响应式资源地址 | string | - |
 | alt | 图像无法显示时的替代文本 | string | - |
+| gap | 字符类型距离左右两侧边界单位像素 | number | 4 | 3.x |
+| draggable | 图片是否允许拖动 | boolean \| `'true'` \| `'false'` | - | 3.x |
 | loadError | 图片加载失败的事件，返回 false 会关闭组件默认的 fallback 行为 | () => boolean | - |
+
+### Avatar.Group (3.x)
+
+| 参数 | 说明 | 类型 | 默认值 | 版本 |
+| --- | --- | --- | --- | --- |
+| maxCount | 显示的最大头像个数 | number | - |  |
+| maxPopoverPlacement | 多余头像气泡弹出位置 | `top` \| `bottom` | `top` |  |
+| maxStyle | 多余头像样式 | CSSProperties | - |  |
+| size | 设置头像的大小 | number \| `large` \| `small` \| `default` \| { xs: number, sm: number, ...} | `default` | 3.x |
