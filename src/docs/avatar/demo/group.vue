eacad021cf9e4d7d18fe4c4b9a38cbd7e3378d49
tanjinzhou <415800467@qq.com>
2021-05-13 13:02:25 +0800

    fix: avatar

--- src/docs/avatar/demo/group.vue
+++ src/docs/avatar/demo/group.vue
@@ -19,29 +19,29 @@ Avatar group display.
   <a-avatar-group>
     <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
     <a-avatar style="background-color: #f56a00">K</a-avatar>
-    <Tooltip title="Ant User" placement="top">
+    <a-tooltip title="Ant User" placement="top">
       <a-avatar style="background-color: #87d068">
         <template #icon><UserOutlined /></template>
       </a-avatar>
-    </Tooltip>
+    </a-tooltip>
     <a-avatar style="background-color: #1890ff">
       <template #icon><UserOutlined /></template>
     </a-avatar>
   </a-avatar-group>
-  <Divider />
+  <a-divider />
   <a-avatar-group :max-count="2" :max-style="{ color: '#f56a00', backgroundColor: '#fde3cf' }">
     <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
     <a-avatar style="background-color: #1890ff">K</a-avatar>
-    <Tooltip title="Ant User" placement="top">
+    <a-tooltip title="Ant User" placement="top">
       <a-avatar style="background-color: #87d068">
         <template #icon><UserOutlined /></template>
       </a-avatar>
-    </Tooltip>
+    </a-tooltip>
     <a-avatar style="background-color: #1890ff">
       <template #icon><UserOutlined /></template>
     </a-avatar>
   </a-avatar-group>
-  <Divider />
+  <a-divider />
   <a-avatar-group
     :max-count="2"
     size="large"
@@ -52,11 +52,11 @@ Avatar group display.
   >
     <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
     <a-avatar style="background-color: #1890ff">K</a-avatar>
-    <Tooltip title="Ant User" placement="top">
+    <a-tooltip title="Ant User" placement="top">
       <a-avatar style="background-color: #87d068">
         <template #icon><UserOutlined /></template>
       </a-avatar>
-    </Tooltip>
+    </a-tooltip>
     <a-avatar style="background-color: #1890ff">
       <template #icon><UserOutlined /></template>
     </a-avatar>
